e2044a938a7472640823440bc7636f702a40a15d
Hassan9001 <117511769+Hassan9001@users.noreply.github.com>
2024-12-15 22:13:01 -0500

    uploaded files

---
+++ model/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+import tensorflow as tf
+
+def dice_coef(y_true, y_pred, smooth=1e-6):
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
